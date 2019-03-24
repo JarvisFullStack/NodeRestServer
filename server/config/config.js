d6bf5ada2e4f00b20633c29568353220fcd4fa40
JarvisXxX <eldrizzy@hotmail.com>
2019-03-24 00:06:26 -0600

    Agregando variables de entorno de bases de datos

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-    urlDB = 'mongodb+srv://jarvis:TG2SRaoCqYDIPgzL@cluster0-s0fza.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
